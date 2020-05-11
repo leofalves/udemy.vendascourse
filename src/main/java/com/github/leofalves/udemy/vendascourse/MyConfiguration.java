5f88f8ef9243375fd41fa581ac97c11084ee5330
Leo Fabiano Alves <leo.fabiano@gmail.com>
2020-05-11 19:22:41 -0300

    Bean

---
+++ src/main/java/com/github/leofalves/udemy/vendascourse/MyConfiguration.java
@@ -0,0 +1,14 @@
+package com.github.leofalves.udemy.vendascourse;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfiguration {
+
+	@Bean(name = "applicationName")
+	public String applicationName() {
+		return "Sistema de vendas do curso Udemy";
+	}
+	
+}
--- src/main/java/com/github/leofalves/udemy/vendascourse/VendascourseApplication.java
+++ src/main/java/com/github/leofalves/udemy/vendascourse/VendascourseApplication.java
@@ -1,5 +1,7 @@
 package com.github.leofalves.udemy.vendascourse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendascourseApplication {
 
+	@Autowired
+	@Qualifier("applicationName")
+	private String applicationName;
+	
+	
 	@GetMapping("/hello")
 	public String HelloWorld() {
-		return "Hello World";
+		return "Hello World from " + applicationName;
 	}
 	
 	public static void main(String[] args) {
