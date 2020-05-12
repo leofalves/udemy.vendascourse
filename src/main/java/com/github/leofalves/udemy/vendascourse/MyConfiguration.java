670d1798e5b1b2c9ae7963c02e01199096aa42bd
Leo Fabiano Alves <leo.fabiano@gmail.com>
2020-05-12 09:53:17 -0300

    Usando Profiles

--- src/main/java/com/github/leofalves/udemy/vendascourse/MyConfiguration.java
+++ src/main/java/com/github/leofalves/udemy/vendascourse/MyConfiguration.java
@@ -1,14 +1,23 @@
 package com.github.leofalves.udemy.vendascourse;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class MyConfiguration {
 
 	@Bean(name = "applicationName")
 	public String applicationName() {
-		return "Sistema de vendas do curso Udemy";
+		return "Sistema de vendas do curso Udemy da classe MyConfiguration";
 	}
 	
+	@Bean
+	public CommandLineRunner executar() {
+		return args -> {
+			System.out.println("Rodando a Configuração de Desenvolvimento");
+		};
+	}
 }
