8adcc0af4dc6f715861185df2bc69cd20f83e752
Leo Fabiano Alves <leo.fabiano@gmail.com>
2020-05-14 16:18:35 -0300

    Mapeamento das Entidades com JPA

--- src/main/java/com/github/leofalves/udemy/vendascourse/domain/entity/Cliente.java
+++ src/main/java/com/github/leofalves/udemy/vendascourse/domain/entity/Cliente.java
@@ -1,10 +1,13 @@
 package com.github.leofalves.udemy.vendascourse.domain.entity;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +21,9 @@ public class Cliente {
 	
 	@Column(name = "nome", length = 100)
 	private String nome;
+	
+	@OneToMany(mappedBy = "cliente")
+	private Set<Pedido> pedidos;
 	
 	public Cliente() {
 	}
@@ -40,6 +46,10 @@ public class Cliente {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public Set<Pedido> getPedidos() {
+		return pedidos;
 	}
 
 
--- src/main/java/com/github/leofalves/udemy/vendascourse/domain/entity/Pedido.java
+++ src/main/java/com/github/leofalves/udemy/vendascourse/domain/entity/Pedido.java
@@ -2,13 +2,38 @@ package com.github.leofalves.udemy.vendascourse.domain.entity;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedido")
 public class Pedido {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
+	
+	@Column(name = "data_pedido")
 	private LocalDate dataPedido;
+	
+	@Column(name = "total", length = 20, precision = 2)
 	private BigDecimal total;
+	
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itens;
 	
 	public Pedido() {
 		
@@ -52,6 +77,12 @@ public class Pedido {
 
 	public void setTotal(BigDecimal total) {
 		this.total = total;
+	}
+	
+	
+
+	public List<ItemPedido> getItens() {
+		return itens;
 	}
 
 	@Override
