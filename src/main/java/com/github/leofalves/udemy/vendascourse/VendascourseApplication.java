9aee57c3a99ddfc2980073bc955010361b1d4694
Leo Fabiano Alves <leo.fabiano@gmail.com>
2020-05-15 18:03:43 -0300

    Requisicao GET com parametros na URL e ResponseEntity

--- src/main/java/com/github/leofalves/udemy/vendascourse/VendascourseApplication.java
+++ src/main/java/com/github/leofalves/udemy/vendascourse/VendascourseApplication.java
@@ -1,11 +1,25 @@
 package com.github.leofalves.udemy.vendascourse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.github.leofalves.udemy.vendascourse.domain.entity.Cliente;
+import com.github.leofalves.udemy.vendascourse.domain.repositories.Clientes;
 
 @SpringBootApplication
 public class VendascourseApplication {
 
+	@Bean
+	public CommandLineRunner commandLineRunner (@Autowired Clientes clientes) {
+		return args -> {
+			 Cliente c1 = new Cliente(null, "Leo Fabiano");
+			 clientes.save(c1);
+		};
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VendascourseApplication.class, args);
 	}
--- src/main/java/com/github/leofalves/udemy/vendascourse/rest/controllers/ClienteController.java
+++ src/main/java/com/github/leofalves/udemy/vendascourse/rest/controllers/ClienteController.java
@@ -1,18 +1,37 @@
 package com.github.leofalves.udemy.vendascourse.rest.controllers;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.github.leofalves.udemy.vendascourse.domain.entity.Cliente;
+import com.github.leofalves.udemy.vendascourse.domain.repositories.Clientes;
+
 @Controller
-@RequestMapping("/api/clientes")
 public class ClienteController {
 	
-	@RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+	private Clientes clientes;
+	
+	public ClienteController(Clientes clientes) {
+		this.clientes = clientes;
+	}
+	
+	@GetMapping("/api/clientes/{id}")
 	@ResponseBody
-	public String helloCliente(@PathVariable("nome") String nomeCliente) {
-		return String.format("Hello %s", nomeCliente);
+	public ResponseEntity getClienteById(@PathVariable Integer id) {
+		Optional<Cliente> cliente = clientes.findById(id);
+		if(cliente.isPresent()) {
+			//ResponseEntity<Cliente> re = new ResponseEntity<>(cliente.get(), HttpStatus.OK);
+			//return re;
+			// As duas linhas acima podem ser substituídas por somente essa abaixo
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
