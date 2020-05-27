deea2f5547161be60565c2cbd7b7d7534d8c1642
Leo Fabiano Alves <leo.fabiano@gmail.com>
2020-05-27 15:40:20 -0300

    Autorizacao em URLs

--- src/main/java/com/github/leofalves/udemy/vendascourse/config/SecurityConfig.java
+++ src/main/java/com/github/leofalves/udemy/vendascourse/config/SecurityConfig.java
@@ -26,15 +26,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.withUser("USER01")
 			.password(this.passwordEncoder().encode("123456"))
 			.roles("USER");
+			//.authorities("MANTER CLIENTE");
 	}
 	
 	
 	// AUTORIZAÇÃO
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
-
+		http.csrf().disable()
+			.authorizeRequests()
+				.antMatchers("/api/clientes/**")
+				.authenticated() 						// SOMENTE USUÁRIOS AUTENTICADOS
+				//.hasRole("USER"); 					// USUÁRIOS AUTENTICADOS E COM A ROLE: USER
+				//.hasAuthority("MANTER CLIENTE");		// USUÁRIOS AUTENTICADOS E COM A AUTHORITY: MANTER CLIENTE
+				//.permitAll();							// PERMITIDO PARA TODOS (SEM SEGURANÇA)
+			.and()
+				//.formLogin("/meu-login.html"); 			// PODE TER UM FORMULARIO PERSONALIZADO OU O DEFAULT DO SPRING
+				.formLogin();
+		
+			/*
+			 * No formulário de login personalizado precisa de um form conforme abaixo:
+			 * <form method="post">
+			 * 		<input type="text" name="username">
+			 * 		<input type="secret" name="password">
+			 * 		<button type="submit">
+			 * </form>
+			 * 
+			 * */
+	
 	}
-
-
 }
