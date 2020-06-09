d0152e002fece57762e4ce7872583c8f7a911069
Leo Fabiano Alves <leo.fabiano@gmail.com>
2020-06-09 14:37:57 -0300

    Fazer Build com profile no pom.xml

--- src/main/java/com/github/leofalves/udemy/vendascourse/VendascourseApplication.java
+++ src/main/java/com/github/leofalves/udemy/vendascourse/VendascourseApplication.java
@@ -2,9 +2,10 @@ package com.github.leofalves.udemy.vendascourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VendascourseApplication {
+public class VendascourseApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendascourseApplication.class, args);
